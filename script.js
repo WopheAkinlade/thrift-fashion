1eb9c15c70c8a0d4f3ee42451e77a58b0d5f1820
Olaniran Micheal <olaniranmicheal2020@gmail.com>
2022-08-12 11:54:25 +0100

    Product page  structured

---
+++ script.js
@@ -0,0 +1,33 @@
+
+let http = new XMLHttpRequest();
+
+http.open('get','data.json', true)
+
+http.send();
+
+http.onload =function(){
+    if(this.readyState == 4 && this.status == 200){
+        let products = JSON.parse(this.responseText);
+
+        let output = '';
+
+        for (let item of products){
+            output +=`
+            <div class='product'>
+                <img src ="${item.productsImages}" alt="" >
+                <p class ="title">${item.producType}</p>
+                <p class ="gender">${item.gender}</p>
+                <p class ="price">
+                    <span>${item.price}</span>
+                    <span>&euro;</span>
+                </p>
+                <button class='cart'>Add to cart</button>
+            </div>
+            `
+        }
+
+        document.querySelector(".products").innerHTML = output;
+        console.log(products)
+    }
+}
+
